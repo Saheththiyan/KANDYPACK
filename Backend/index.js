d9916cd31f6408e265ed217603eb9d68905684ca
Sahe <saheththiyans@gmail.com>
2025-09-11 01:59:12 +0530

    Setting up

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
