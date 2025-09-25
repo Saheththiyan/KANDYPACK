1ba9fa11948853c3fd636a3ad939ebb2c2e191f2
Buvindu Suraweera <buvindudsuraweera@gmail.com>
2025-09-25 17:03:45 +0530

    Updated driver endpoint

--- Backend/models/driverModel.js
+++ Backend/models/driverModel.js
@@ -4,3 +4,28 @@ export async function getDrivers() {
   const [drivers] = await db.query("SELECT * FROM driver");
   return drivers;
 }
+
+export async function getDriversByID(id) {
+  const [driver] = await db.query(
+    `SELECT * FROM driver
+    WHERE driver_id = ?`,
+    [id]
+  );
+  return driver;
+}
+
+export async function sortDriversByHour() {
+  const [drivers] = await db.query(
+    `SELECT * FROM driver
+    ORDER BY weekly_hours DESC`
+  );
+  return drivers;
+}
+
+export async function getActiveDrives() {
+  const [drivers] = await db.query(
+    `SELECT * FROM driver
+    WHERE status = "active" `
+  );
+  return drivers;
+}
